00bca2dad8941d293a8708e6c25b71a6932b9b70
tannuu941 <tannuu941@outlook.com>
2024-07-17 15:34:06 +0400

    Update config-overrides.js bug fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,8 @@
 
-/** Set the configuration value the following that: 
- * 
-*/
 // export const BASE_URL = "http://87.237.52.168:3000";
-export const BASE_URL = "http://localhost:3002";
+//export const BASE_URL = "http://localhost:3002";
 // export const BASE_URL = "https://dispossessor.com/back";
-export const SOCKET_URL = "http://localhost:3003";
+//export const SOCKET_URL = "http://localhost:3003";
 
 // query getOffers{
 //     offers {
